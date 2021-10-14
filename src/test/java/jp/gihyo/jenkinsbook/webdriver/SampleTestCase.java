20122aacf44537c0ff575fb74e387b82c3a582a7
shirahata-yuya <88171828+shirahata-yuya@users.noreply.github.com>
2021-10-14 21:08:33 +0900

    Update SampleTestCase.java

--- src/test/java/jp/gihyo/jenkinsbook/webdriver/SampleTestCase.java
+++ src/test/java/jp/gihyo/jenkinsbook/webdriver/SampleTestCase.java
@@ -25,10 +25,7 @@ public class SampleTestCase {
 	@BeforeClass
 	public static void setUpClass() throws IOException {
 		prop.load(new FileInputStream("src/test/resources/selenium.properties"));
-		
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/msedgedriver");
-		WebDriver driver = new ChromeDriver();
-		
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/msedgedriver");	
 		DesiredCapabilities m_capability = DesiredCapabilities.edge();
 		driver = new ChromeDriver(m_capability);
 	}
